677359eb827fae33e113bd62bb7f9cffd3c51651
TrpFrog <dev@trpfrog.net>
2022-03-02 23:58:06 +0900

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type {NextPage} from 'next'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import {ArticleData, fetchBlogData} from "../lib/articles";
 import Layout from "../components/Layout";
 import BlogCard from "../components/BlogCard";
+import {NextSeo} from "next-seo";
 
 type Props = {
     data: ArticleData[]
@@ -21,6 +22,10 @@ export const getStaticProps = async () => {
 const Home: NextPage<Props> = ({data}) => {
     return (
         <Layout>
+            <NextSeo
+                title="Walkicles"
+                description="徒歩記事まとめサイト"
+            />
             <div id={styles.card_grid}>
                 {data.map(e => (
                     <BlogCard data={e} key={e.id}/>
